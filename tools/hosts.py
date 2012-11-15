07e0fa2500e9fcfc147f0c2a85501566283f3c90
xtso520ok <xtso520ok@gmail.com>
2012-11-15 15:33:39 +0800

    增加修改本地hosts功能

--- tools/hosts.py
+++ tools/hosts.py
@@ -2,17 +2,19 @@
 # -*- coding:UTF-8 -*-
 # filename:hosts.py
 
-import web
+import web, re
+from _pyio import open
 
 urls = (
     '/index', 'index',
+    '/mod', 'mod',
     '(.*)', 'rehosts'
 )
 
 app_hosts = web.application(urls, globals())
 render_hosts = web.template.render('templates/')
 
-hostsfile = '/etc/hosts'
+hostsFile = '/etc/hosts'
 
 class rehosts:
     def GET(self, path):
@@ -20,9 +22,37 @@ class rehosts:
 
 class index:
     def GET(self):
-        r = open(hostsfile)
-        hostlist = r.readlines()
-        print len(hostlist)
-        for i in hostlist:
-            print i
-        return 'OK'
+        web.header('Content-Type', 'text/html; charset=utf-8', unique=True)
+        r = open(hostsFile)
+        tmpList = r.readlines()
+        r.close()
+        responseHtml = '<form method="post" action="/hosts/mod">'
+        hosts = ''
+        for host in tmpList:
+            isMatch = bool(re.match(r'^\#?[0-9]+(\.[0-9]+){3}.*$', host, re.VERBOSE));
+            if isMatch: 
+                hosts += host
+        responseHtml += '<textarea name="hosts" cols="60" rows="15">' + hosts + '</textarea><br/>'
+        responseHtml += '<input type="submit" value="更新"></form>'
+        return responseHtml
+    
+class mod:
+    def POST(self):
+        i = web.input()
+        w = open(hostsFile, 'w')
+        w.writelines(i.hosts)
+        w.close()
+        # 响应
+        web.header('Content-Type', 'text/html; charset=utf-8', unique=True)
+        r = open(hostsFile)
+        tmpList = r.readlines()
+        r.close()
+        responseHtml = '<form method="post" action="/hosts/mod">'
+        hosts = ''
+        for host in tmpList:
+            isMatch = bool(re.match(r'^\#?[0-9]+(\.[0-9]+){3}.*$', host, re.VERBOSE));
+            if isMatch: 
+                hosts += host
+        responseHtml += '<textarea name="hosts" cols="60" rows="15">' + hosts + '</textarea><br/>'
+        responseHtml += '<input type="submit" value="更新"></form>'
+        return responseHtml
