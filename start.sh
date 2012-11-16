79797f1e9f3f928ff4d3642f9d928cabee284bd5
xtso520ok <xtso520ok@gmail.com>
2012-11-16 14:27:03 +0800

    更新常规启动项start.sh

--- start.sh
+++ start.sh
@@ -4,6 +4,6 @@ PATH=/home/david/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bi
 port=9002
 #ps ax | grep $port | grep -v "grep" | awk '{print $2}' | xargs kill
 homedir=/home/david/Publish/itsys
-python $homedir/start.py 127.0.0.1:$port >/dev/null 2>&1 &
+python $homedir/index.py 127.0.0.1:$port >/dev/null 2>&1 &
 
 exit 0
